adebe42f92228caa2dd2b2030ad7e506243a18ee
Malvvern Gondo <malvernbright.95@gmail.com>
2026-04-16 23:11:11 +0200

    chore: update code structure for better readability and maintainability

---
+++ ClassificationTree.R
@@ -0,0 +1,34 @@
+df <- read.csv("Movie_classification.csv")
+View(df)
+
+# Data Preprocessing
+summary(df)
+df$Time_taken[is.na(df$Time_taken)] <- mean(df$Time_taken, na.rm = TRUE)
+summary(df)
+
+# Test-Train Split
+install.packages("caTools")
+library(caTools)
+set.seed(0)
+split <- sample.split(df, SplitRatio = .8)
+trainc <- subset(df, split = TRUE)
+testc <- subset(df, split = FALSE)
+
+## Class Tree
+install.packages("rpart")
+install.packages("rpart.plot")
+library("rpart")
+library("rpart.plot")
+
+## Run regression tree model on train set
+classtree <- rpart(formula = Start_Tech_Oscar~., data = trainc, method = 'class', control = rpart.control(maxdepth = 3))
+# press F1 on rpart for help on this function
+
+# Plot the decision tree
+rpart.plot(classtree, box.palette = "RdBu", digits = -3)
+
+# Predict value at any point
+testc$pred <- predict(classtree, testc, type = "class")
+
+table(testc$Start_Tech_Oscar, testc$pred)
+
